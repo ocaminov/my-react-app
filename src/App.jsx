6aa4580a69492d595127d411c91a6833465891af
Oleyda Del Camino <ocaminov@gmail.com>
2026-09-01 18:43:38 -0400

    Render movies dynamically with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
+import "./App.css";
+import MovieCard from "./MovieCard";
 
-import './App.css'
-import MovieCard from './MovieCard'
-import Welcome from './Welcome'
-
+const movies = [
+  {
+    title: "Inception",
+    year: 2010,
+    rating: 8.8,
+  },
+  {
+    title: "Interstellar",
+    year: 2014,
+    rating: 8.7,
+  },
+  {
+    title: "Avatar",
+    year: 2009,
+    rating: 7.8,
+  },
+];
 
 function App() {
-  return <>
-  <MovieCard title="Inception" year="2010" rating="8.8"/>
-  <MovieCard title="Interstellar" year="2014" rating="8.7"/>
-  <MovieCard title="Avatar" year="2009" rating="7.8"/>
-  </>
-  
+  return (
+    <>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.title}
+          title={movie.title}
+          year={movie.year}
+          rating={movie.rating}
+        />
+      ))}
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,11 +1,11 @@
-function MovieCard({title, year, rating}){
-    return (
-        <>
-            <h3>{title}</h3>
-            <p>{year}</p>
-            <p>{rating}</p>
-        </>
-    );
+function MovieCard({ title, year, rating }) {
+  return (
+    <>
+      <h3>{title}</h3>
+      <p>{year}</p>
+      <p>⭐{rating}</p>
+    </>
+  );
 }
 
 export default MovieCard;
