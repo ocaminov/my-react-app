1888902883d140a036c6e25fb78971eeaf54de99
Oleyda Del Camino <ocaminov@gmail.com>
2026-09-01 18:32:33 -0400

    Create reusable MovieCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 import './App.css'
+import MovieCard from './MovieCard'
 import Welcome from './Welcome'
 
 
 function App() {
-  return <Welcome />
+  return <>
+  <MovieCard title="Inception" year="2010" rating="8.8"/>
+  <MovieCard title="Interstellar" year="2014" rating="8.7"/>
+  <MovieCard title="Avatar" year="2009" rating="7.8"/>
+  </>
   
 }
 
